eedb4704ba9eeb00051e35fdfb1b50702869b64a
MuhammadShahzaibKhan <mshahzaibkhan17@gmail.com>
2023-08-19 10:31:24 +0500

    Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import icon from "./images/icon.png";
+import "./App.css";
 
 function App() {
+  let [value, setValue] = useState("");
+  let [list, setList] = useState([]);
+
+  let add = () => {
+    list.push(value);
+    setList([...list]);
+    setValue("");
+  };
+
+  let handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="main">
+          <div className="Todo-app">
+            <h3>
+              To-Do List <img src={icon} />
+            </h3>
+            <div className="input-main">
+              <input
+                type="text"
+                placeholder="Enter your Task"
+                value={value}
+                className="input-box"
+                onChange={(e) => setValue(e.target.value)}
+              />
+              <button type="submit" onClick={add}>
+                Add Task
+              </button>
+            </div>
+            <div className="list-main">
+              {list.map((x, i) => (
+                <li key={i} className="list">
+                  {x}{" "}
+                  <button
+                    className="editbtn"
+                    onClick={(e) =>
+                      console.log(
+                        (e.target.parentNode.firstChild.nodeValue =
+                          prompt("Edit your task"))
+                      )
+                    }
+                  >
+                    Edit
+                  </button>
+                  <button
+                    className="deletebtn"
+                    onClick={(e) => e.target.parentNode.remove()}
+                  >
+                    Delete
+                  </button>{" "}
+                </li>
+              ))}
+            </div>
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
